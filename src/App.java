32bc8fdc4353383bfabaea1e9db1f0adfd0e66f5
r4m0nl1m4 <ramon.ccodes@gmail.com>
2023-07-01 19:00:01 -0300

    Serviço de limpeza. Refatoração de tratamentos de excessões e println's.

--- src/App.java
+++ src/App.java
@@ -2,59 +2,70 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {        
         Scanner sc = new Scanner(System.in);
         Hotel hotelIMD = new Hotel();
 
         int opt = 1;
         do {
             try {
-                System.out.println();
-                System.out.println("--- Bem-vindo ao sistema de reservas do Hotel IMD ---");
-                System.out.println("Digite 1 para listar funcionários");
-                System.out.println("Digite 2 para listar dados de um funcionário pelo seu nome");
-                System.out.println("Digite 3 para listar hóspedes");
-                System.out.println("Digite 4 para listar dados de um hóspede pelo seu nome");
-                System.out.println("Digite 5 para demandar serviços de recursos humanos");
-                System.out.println("Digite 6 para demandar serviços de recepção");
-                System.out.println("Digite 7 para demandar serviços de limpeza de quarto");
-                System.out.println("Digite 8 para demandar serviços de restaurante");
-                System.out.println("Digite 9 para finalizar o programa");
+                System.out.print(  "\n--- Sistema de Reservas do Hotel IMD ---\n"
+                                 + "\nOpções:"
+                                 + "\n\t [1] Listar funcionários"
+                                 + "\n\t [2] Listar dados de um funcionário pelo seu nome"
+                                 + "\n\t [3] Listar hóspedes"
+                                 + "\n\t [4] Listar dados de um hóspede pelo seu nome"
+                                 + "\n\t [5] Demandar serviços de recursos humanos"
+                                 + "\n\t [6] Demandar serviços de recepção"
+                                 + "\n\t [7] Demandar serviços de limpeza de quarto"
+                                 + "\n\t [8] Demandar serviços de restaurante"
+                                 + "\n\t [0] Finalizar o programa"
+                                 + "\nDigite sua opção [0-8]: ");
+
                 opt = sc.nextInt();
 
-                switch (opt) {
-                    case 1:
-                        hotelIMD.imprimirFuncionarios();
-                        break;
-                    case 2:
-                        hotelIMD.imprimirFuncionario(sc);
-                        break;
-                    case 3:
-                        hotelIMD.imprimirHospedes();
-                        break;
-                    case 4:
-                        hotelIMD.imprimirHospede(sc);
-                        break;
-                    case 5:
-                        hotelIMD.demandarServicosRH(sc);
-                        break;
-                    case 6:
-                        hotelIMD.demandarServicosRecepcao(sc);
-                        break;
-                    case 7:
-                    	//hotelIMD.demandarServicosLimpeza(sc);
-                    	break;
-                    case 8:
-                    	hotelIMD.demandarServicoRestaurante(sc);
-                        break;
-                    default:
-                        break;
+                System.out.println();
+
+                if(opt < 0 || opt > 8) { 
+                    System.out.println("Opção inválida! Tente novamente.");
+                } else {
+                    switch (opt) {
+                        case 0:
+                            System.out.println("Programa finalizado.");
+                            break;
+                        case 1:
+                            hotelIMD.imprimirFuncionarios();
+                            break;
+                        case 2:
+                            hotelIMD.imprimirFuncionario(sc);
+                            break;
+                        case 3:
+                            hotelIMD.imprimirHospedes();
+                            break;
+                        case 4:
+                            hotelIMD.imprimirHospede(sc);
+                            break;
+                        case 5:
+                            hotelIMD.demandarServicosRH(sc);
+                            break;
+                        case 6:
+                            hotelIMD.demandarServicosRecepcao(sc);
+                            break;
+                        case 7:
+                            hotelIMD.demandarServicosLimpeza(sc);
+                            break;
+                        case 8:
+                            hotelIMD.demandarServicoRestaurante(sc);
+                            break;
+                        default:
+                            break;
+                    }                
                 }
             } catch (InputMismatchException e) {
-                System.out.println("Opção inválida " + e);
+                System.out.println("\nOpção inválida! Error: " + e);
                 break;
             }
-        } while (opt != 9);
+        } while (opt != 0);
         sc.close();
     }
 }
--- src/Camareira.java
+++ src/Camareira.java
@@ -1,9 +1,64 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Camareira extends Funcionario {
     public Camareira(String nome, byte idade, float salario, Date dataAdmissao) {
         super(nome, idade, salario, dataAdmissao);
         //TODO Auto-generated constructor stub
+    }
+
+    static public void solicitarLimpeza(Scanner sc) {
+        try {        
+
+            System.out.print("Digite o número do quarto: ");
+            int numeroQuarto = sc.nextInt();
+
+            System.out.println();
+
+            boolean quartoExiste = false; 
+            for (Quarto q : Hotel.quartos) {
+                if ( q.getNumero() == numeroQuarto ) quartoExiste = true;
+            }
+
+            if(quartoExiste) {
+
+                SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+                System.out.print("Digite a data de agendamento (dd/MM/yyyy): ");
+                Date data = sdf.parse(sc.next());
+
+                System.out.println(  "Limpeza do quarto " + numeroQuarto
+                                   + " agendada para o dia " + data + ".");
+            } else {
+                System.out.println("Quarto inexistente.");
+            }
+
+        } catch (ParseException | InputMismatchException | IllegalArgumentException e) {
+            System.out.println("\nOpção inválida! Error: " + e);
+        }
+    }
+
+    static public void cancelarLimpeza(Scanner sc) {
+        System.out.print("Digite o número do quarto: ");
+        int numeroQuarto = sc.nextInt();
+
+        System.out.println();
+
+        boolean quartoExiste = false; 
+        for (Quarto q : Hotel.quartos) {
+            if ( q.getNumero() == numeroQuarto ) {
+                System.out.println("Limpeza do quarto " + q.getNumero() + " cancelada.");
+                quartoExiste = true;
+            }
+        }
+
+        if(quartoExiste) {
+            System.out.println(  "Agendamento da limpeza do quarto " + numeroQuarto + " cancelado.");
+        } else {
+            System.out.println("Quarto inexistente.");
+        }
     }
 
     public void limparQuarto(Quarto quarto) {
